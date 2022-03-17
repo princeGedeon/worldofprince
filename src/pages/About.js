7b97d4975c497c70e9935256897fcb9697a27a38
princeGedeon <guedjegedeon03@gmail.com>
2022-03-17 20:38:16 +0100

    Ajout des informations sur About us

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,10 +9,17 @@ export default function() {
         <Navigation/>
         <h1>About</h1>
         <br/>
+        <div style="margin:15px;padding:15px;">
+        <h1>Prince Gédéon GUEDJE</h1>
+        <h3> Nice to meet you! 👋</h3>
         <p> 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sed ipsa harum in quam voluptate? Voluptatum delectus soluta molestiae ullam. Iste, nihil. Veniam exercitationem rem, minus eligendi labore tempore animi?
+       
+Machine Learning Engineer,Developper Python,FullStack Django and React JS
+I have been passionate about computer Science in general, for many years. The fact to create applications, to configure systems, to control remote machines are are all things that pushed me into this domain.
 
+I am amazed by Artificial Intelligence, the creation of neural networks with Deep Learning to solve difficult problems that escape classical programming. My future career and my professional goal is to become a Machine Learning Engineer
         </p>
+        </div>
     </div>
   )
 }
